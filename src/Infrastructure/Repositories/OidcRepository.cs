39ad0a7d8d9380a9463867a492eec7bec9e63e03
javierhersan <javier.hernandezsanchez@outlook.com>
2025-12-21 23:23:29 +0100

    chore: add oidc repository comments

--- src/Infrastructure/Repositories/OidcRepository.cs
+++ src/Infrastructure/Repositories/OidcRepository.cs
@@ -4,3 +4,6 @@
 // Adds also OPEID Discovery endpoint: https://your-auth-server/.well-known/openid-configuration
 // Adds also UserInfo endpoint to get more user claims (beyond ID token): https://your-auth-server/userinfo
 // UserInfo endpoint is called by the client application after getting the ID token and access token.
+
+// OAuth Access Token and Refresh Token generation should not contain OIDC-specific claims (like 'sub', 'name', 'email') to not reveal sensitive user information.
+// Sub can be exposed (User ID) but avoid adding more claims in access token for better security and privacy.
